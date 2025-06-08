2411ad0e4fc78afeff4ceb607e31310c56310e66
emrepbu <emrewindowsarg@gmail.com>
2025-06-08 17:17:57 +0300

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by emre argana on 8.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
